9eba51a3463a947aed4c3527b09cf6a3f0f1bc93
Hong Ma <hma@thoughtworks.com>
2018-03-19 09:44:48 +0800

    add static style

--- src/main/java/io/honeymon/springboot/jspsample/web/RootController.java
+++ src/main/java/io/honeymon/springboot/jspsample/web/RootController.java
@@ -1,16 +1,19 @@
 package io.honeymon.springboot.jspsample.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RootController {
 
     @RequestMapping("/")
-    public String helloWorld() {
+    public String helloWorld(Model model) {
+
+        model.addAttribute("hello", "dfkas 111");
         return "hello";
     }
-    
+
     @RequestMapping("/2nd")
     public String secondSubPage() {
         return "test/list";
